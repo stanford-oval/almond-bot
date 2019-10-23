5ac32c2297bf1de52cc3c888431a727101389fc5
Euirim Choi <euirim@me.com>
2019-10-23 07:50:53 -0700

    Comment out extraneous command

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SUBSCRIPTION="Free Trial"
 RESOURCE_GROUP_NAME=Almond
-WEB_APP_NAME=almond-bot
+WEB_APP_NAME="almond-bot"
 APP_ID=$ALMOND_APP_ID
 APP_PASSWORD=$ALMOND_APP_PASSWORD
 
@@ -9,7 +9,8 @@ yarn build
 
 az login
 az account set --subscription "${SUBSCRIPTION}"
-az bot create --kind webapp --resource-group "${RESOURCE_GROUP_NAME}" --name "${WEB_APP_NAME}" --appid "${APP_ID}" --password "${APP_PASSWORD}" --tags "connectionName=Almond" --lang Javascript
+#az ad app create --display-name "almond-bot" --password "m(UW9N;-g[WPXDNa" --available-to-other-tenants
+az bot create --kind webapp --resource-group "${RESOURCE_GROUP_NAME}" --name "${WEB_APP_NAME}" --appid "${APP_ID}" --password "${APP_PASSWORD}" --lang Javascript
 rm -f web.config
 az bot prepare-deploy --code-dir "." --lang Typescript
 rm -f code.zip
