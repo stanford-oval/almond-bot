fa6e9c924a5a2373eceffb7725ff445e48f6e0ed
Euirim Choi <euirim@me.com>
2019-10-23 04:50:28 -0700

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SUBSCRIPTION="Free Trial"
+RESOURCE_GROUP_NAME=Almond
+WEB_APP_NAME=almond-bot
+APP_ID=$ALMOND_APP_ID
+APP_PASSWORD=$ALMOND_APP_PASSWORD
+
+yarn build
+
+az login
+az account set --subscription "${SUBSCRIPTION}"
+az bot create --kind webapp --resource-group "${RESOURCE_GROUP_NAME}" --name "${WEB_APP_NAME}" --appid "${APP_ID}" --password "${APP_PASSWORD}" --tags "connectionName=Almond" --lang Javascript
+rm src/web.config
+az bot prepare-deploy --code-dir "./src" --lang Typescript
+zip -r code.zip .
+az webapp deployment source config-zip --resource-group "${RESOURCE_GROUP_NAME}" --name "${WEB_APP_NAME}" --src "code.zip"
